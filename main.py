27f8ce794f1529b416bfee10e85ac0e8e5e8ce83
Lucas <134971879+loadedlucas@users.noreply.github.com>
2026-06-18 18:59:07 +0200

    Upload

---
+++ main.py
@@ -0,0 +1,157 @@
+import certifi
+import os
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+
+import discord
+from discord import app_commands, Embed
+from discord.ext import commands, tasks
+import logging
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, time
+
+import database
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# Definitions
+PETITIONS_CHANNEL_ID = 1514421813231222857
+VOTES_CHANNEL_ID = 1514421992281870407
+VOTING_TIME = time(hour=18, minute=0, tzinfo=datetime.now().astimezone().tzinfo)
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+database.init_db()
+
+async def push_pending_petitions():
+    votes_channel = bot.get_channel(VOTES_CHANNEL_ID)
+    petitions = database.get_pending_petitions()
+    for i in petitions:
+        petition_type = "petition" if i[1] == "Other" else i[1]
+
+        # Create Poll
+        poll = discord.Poll(question=f"World Vote #{i[0]}: Approve this {petition_type}?", duration=timedelta(hours=72))
+        poll.add_answer(text="👍 Yes")
+        poll.add_answer(text="👎 No")
+
+        poll_message = await votes_channel.send(poll=poll)
+        await poll_message.reply(f"**Discussion thread:** {i[2]}")
+
+        database.add_to_voted(i[0])
+
+@bot.event
+async def on_ready():
+    if not push_petition_task.is_running():
+        push_petition_task.start()
+    print(f"{bot.user} is online!")
+    await bot.tree.sync()
+
+# ----- Create Petition -----
+@bot.tree.command()
+# Fetch petition data
+@app_commands.describe(title="Short, descriptive and specific")
+@app_commands.describe(location="Exact coordinates, region name, or clear boundaries")
+@app_commands.choices(type=[
+    app_commands.Choice(name="Rule Change", value="Rule Change"),
+    app_commands.Choice(name="Shop Takeover", value="Shop Takeover"),
+    app_commands.Choice(name="Shop Expansion", value="Shop Expansion"),
+    app_commands.Choice(name="Build Takeover", value="Build Takeover"),
+    app_commands.Choice(name="Build Removal", value="Build Removal"),
+    app_commands.Choice(name="Exception", value="Exception"),
+    app_commands.Choice(name="Reservation", value="Reservation"),
+    app_commands.Choice(name="Infrastructure Change", value="Infrastructure Change"),
+    app_commands.Choice(name="Other", value="Other")
+])
+@app_commands.describe(state="What exists now? Who uses it? Why is this an issue?")
+@app_commands.describe(proposal="Exactly what would change if this passes")
+@app_commands.describe(impact="Who is affected? Positives and tradeoffs")
+@app_commands.describe(scope="What this does NOT affect; any boundaries or constraints")
+@app_commands.describe(duration="Permanent / Temporary / Trial period, if applicable")
+@app_commands.describe(maintenance="Who maintains it? What happens if it's abandoned?")
+@app_commands.describe(alternatives="What other solutions have been considered?")
+@app_commands.choices(acknowledgement=[
+    app_commands.Choice(name="I understand that approval is not guaranteed and that the outcome of this vote is final", value="I understand that approval is not guaranteed and that the outcome of this vote is final")
+])
+async def petition(
+    interaction: discord.Interaction,
+    title: str,
+    location: str,
+    type: str,
+    state: str,
+    proposal: str,
+    impact: str,
+    scope: str,
+    duration: str,
+    maintenance: str,
+    alternatives: str,
+    acknowledgement: str
+):
+    # Save petition to database
+    user_id = interaction.user.id
+    petition_id = database.add_petition(type=type)
+
+    # Fetch @user
+    try:
+        user = await bot.fetch_user(user_id)
+        user_mention = user.mention
+    except discord.NotFound:
+        user_mention = f"<@{user_id}>"
+
+    # Send petition in "#petitions"
+    petitions_channel = bot.get_channel(PETITIONS_CHANNEL_ID)
+    
+    embed = Embed(
+        title=f"Petition #{petition_id}: {title}",
+        description=f"**Submitted by:** {user_mention}"
+    )
+    embed.add_field(name="Location", value=location, inline=True)
+    embed.add_field(name="Type", value=type, inline=True)
+    embed.add_field(name="State", value=state, inline=False)
+    embed.add_field(name="Proposal", value=proposal, inline=False)
+    embed.add_field(name="Impact", value=impact, inline=False)
+    embed.add_field(name="Scope", value=scope, inline=False)
+    embed.add_field(name="Duration", value=duration, inline=False)
+    embed.add_field(name="Maintenance", value=maintenance, inline=False)
+    embed.add_field(name="Alternatives", value=alternatives, inline=False)
+    embed.add_field(name="Acknowledgement", value=acknowledgement, inline=False)
+
+    petition_message = await petitions_channel.send(embed=embed)
+
+    thread = await petition_message.create_thread(
+        name=f"Petition #{petition_id}: {title}",
+        reason=f"Discussion thread for petition #{petition_id}"
+    )
+
+    database.update_thread_link(thread_link=thread.mention, petition_id=petition_id)
+    await interaction.response.send_message(f"Petition saved for review. Screenshots can be added in the discussion thread {thread.mention}. Votes will start on Friday!", ephemeral=True)
+
+# ----- Delete Petitions -----
+@bot.tree.command()
+@app_commands.checks.has_any_role("Moderator", "Admin")
+async def deletepetition(interaction: discord.Interaction, petition_id: int):
+    database.hold_back(petition_id=petition_id)
+    await interaction.response.send_message(f"Petition #{petition_id} has been removed from the queue! It will not appear in the next voting round.", ephemeral=True)
+
+# ----- Push Petitions -----
+@bot.tree.command()
+@app_commands.checks.has_permissions(administrator=True)
+async def push_petitions(interaction: discord.Interaction):
+    await interaction.response.defer(ephemeral=True)
+    votes_channel = bot.get_channel(VOTES_CHANNEL_ID)
+    await push_pending_petitions()
+    await interaction.followup.send(f"Petitions now put up for vote in {votes_channel.mention}!", ephemeral=True)
+
+# ----- Automated Petitions every Friday -----
+@tasks.loop(time=VOTING_TIME)
+async def push_petition_task():
+    if datetime.now().weekday() != 4:
+        return
+    await push_pending_petitions()
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
